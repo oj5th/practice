4cba49c06daba2ca69baafeaed80c00ef5d5c955
OJ De Quintos <2008oliverjesus@gmail.com>
2022-02-07 21:46:06 +0800

    Initial rspec and factorybot

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Customer, type: :model do
+  describe "test" do
+    let(:customer) { build_stubbed :customer }
+
+    it "it" do
+      binding.pry
+    end
+  end
+end
