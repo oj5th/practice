36aecf2ea120c12d720b5bd11b7f3feb7f448a9c
OJ De Quintos <2008oliverjesus@gmail.com>
2022-03-30 13:36:11 +0800

    Enabling the Trackable Module

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -9,6 +9,11 @@ class RolesController < ApplicationController
 
   # GET /roles/1 or /roles/1.json
   def show
+    if @role.users.empty?
+      @associated_users = 'None'
+    else
+      @associated_users = @role.users.map(&:name).join(', ')
+    end
   end
 
   # GET /roles/new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  # rails generate migration AddDeviseTrackableColumnsToUsers # when adding :trackable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
   belongs_to :role
   has_many :items, dependent: :destroy
   validates :name, presence: true
