6155f2176c97fd8229bd8ce58fe04fb19775ea95
OJ De Quintos <2008oliverjesus@gmail.com>
2022-03-30 12:56:24 +0800

    Customizing Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   belongs_to :role
   has_many :items, dependent: :destroy
+  validates :name, presence: true
+  before_save :assign_role
+
+  def assign_role
+    self.role = Role.find_by name: 'Regular' if role.nil?
+  end
 end
